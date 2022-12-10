51f545754fd16fc064623dbc39dde98a240e0873
PedamalluAbhiram <ugs207145_it.abhiram@cbit.org.in>
2022-12-10 15:26:53 +0530

    Made some changes

--- server/server.js
+++ server/server.js
@@ -1,2 +1,18 @@
 const express =require('express');
 const colors=require('colors');
+const mongoose =require ('mongoose');
+const PORT=process.env.PORT || 5000;
+app.use=express();
+app.use(express.json({extended:false}));
+
+const connectDB = async()=> {
+    try {
+        const conn =await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+    }
+    catch (err){
+        console.log(`ERROR: ${err.message}`)
+    }
+}
+
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`.bgCyan.underline.bold));
